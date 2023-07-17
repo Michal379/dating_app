cc64ae55425189adc3e1b17fcd31496ccfddb0bd
Michal <khakaimic4@gmail.com>
2023-07-18 00:43:25 +0300

    chats & matches controllers

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,45 @@
 class ChatsController < ApplicationController
+  def index
+    chats = Chat.all
+    render json: chats.to_json(except: [:created_at, :updated_at])
+  end
+
+  def show
+    chat = find_chat
+    render json: chat.to_json(except: [:created_at, :updated_at])
+  end
+
+  def create
+    chat = Chat.new(chat_params)
+    if chat.save
+      render json: chat.to_json(except: [:created_at, :updated_at]), status: :created
+    else
+      render json: { error: "Failed to create chat" }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    chat = find_chat
+    if chat.update(chat_params)
+      render json: chat.to_json(except: [:created_at, :updated_at])
+    else
+      render json: { error: "Failed to update chat" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    chat = find_chat
+    chat.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_chat
+    Chat.find_by(id: params[:id])
+  end
+
+  def chat_params
+    params.require(:chat).permit(:user_id, :message, :read, :sent_at)
+  end
 end
--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,57 @@
 class MatchesController < ApplicationController
+  before_action :authenticate_admin, except: [:index, :show]
+
+  def index
+    matches = Match.all
+    render json: matches.to_json(except: [:created_at, :updated_at])
+  end
+
+  def show
+    match = find_match
+    render json: match.to_json(except: [:created_at, :updated_at])
+  end
+
+  def create
+    match = Match.new(match_params)
+    if match.save
+      render json: match.to_json(except: [:created_at, :updated_at]), status: :created
+    else
+      render json: { error: "Failed to create match" }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    match = find_match
+    if match.update(match_params)
+      render json: match.to_json(except: [:created_at, :updated_at])
+    else
+      render json: { error: "Failed to update match" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    match = find_match
+    match.rooms.destroy_all # Destroy associated rooms before destroying the match
+    if match.destroy
+      head :no_content
+    else
+      render json: { error: "Failed to delete match" }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def authenticate_admin
+    unless current_user&.admin?
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
+  end
+
+  def find_match
+    Match.find_by(id: params[:id])
+  end
+
+  def match_params
+    params.require(:match).permit(:user_id, :status, :match_score, :matched_at)
+  end
 end
