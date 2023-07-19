9b8a770a555353f16fd319756221fa84c3169466
Michal <khakaimic4@gmail.com>
2023-07-19 22:22:01 +0300

    signin and login

--- client/src/components/signin/Signin.jsx
+++ client/src/components/signin/Signin.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Signin = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, setLogin] = useState('');
+  const [errors, setErrors] = useState([]);
 
-export default Signin
+  function onSubmit(e) {
+    e.preventDefault(); // Prevent default form submission behavior
+
+    // Here you can add logic to handle form submission
+    // For this example, let's just demonstrate setting errors if the fields are empty
+    const newErrors = [];
+    if (!name) {
+      newErrors.push('Name is required');
+    }
+    if (!password) {
+      newErrors.push('Password is required');
+    }
+    setErrors(newErrors);
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <label>
+          Name
+          <br />
+          <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
+          Password
+          <br />
+          <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <input type='submit' value='sign up' />
+        <input type='button' value='Login' onClick={() => setLogin('Login clicked')} />
+      </form>
+      {errors && errors.map((error, index) => <p key={index}>{error}</p>)}
+    </>
+  );
+};
+
+export default Signin;
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :matches
   resources :chats
   resources :profiles
-  resources :users
+  resources :users, only:[:show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
