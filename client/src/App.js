b9e6ff0b6780748cdb5871b7a937054703eb8aa4
Michal <khakaimic4@gmail.com>
2023-07-26 16:59:20 +0300

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar'
-import Footer from './components/footer'
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './components/home/Home';
+import Signin from './components/signin/Signin';
 
 function App() {
   const [user, setUser] = useState(null); // Define the setUser function
   const [message, setMessage] = useState('');
- 
+  const [userDetails, setUserDetails] = useState(null);
+
   const handleSignup = (user) => {
     setUserDetails(user);
   };
-  
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  const handleSignin = (user) => {
+    // Add your logic for handling the user signin here
+  };
+
   return (
     <BrowserRouter>
       <Navbar user={user} handleLogout={handleLogout} />
       <Routes>
-        <Route path="/" element={<Home />} />        
-        <Route path="/signup" element={<Signup onSignup={handleSignup} />} />
-        <Route path="/admin" element={<Admin />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin onSignin={handleSignin} />} />
       </Routes>
       <Footer />
       {message && <div className="message">{message}</div>}
--- client/src/components/signin/Signin.jsx
+++ client/src/components/signin/Signin.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button, Error, Input, FormField, Label, Textarea } from "../styles";
 
 function SignUpForm({ onLogin }) {
   const [username, setUsername] = useState("");
@@ -38,67 +37,69 @@ function SignUpForm({ onLogin }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <FormField>
-        <Label htmlFor="username">Username</Label>
-        <Input
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
           type="text"
           id="username"
           autoComplete="off"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-      </FormField>
-      <FormField>
-        <Label htmlFor="password">Password</Label>
-        <Input
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           autoComplete="current-password"
         />
-      </FormField>
-      <FormField>
-        <Label htmlFor="password">Password Confirmation</Label>
-        <Input
+      </div>
+      <div>
+        <label htmlFor="password">Password Confirmation</label>
+        <input
           type="password"
           id="password_confirmation"
           value={passwordConfirmation}
           onChange={(e) => setPasswordConfirmation(e.target.value)}
           autoComplete="current-password"
         />
-      </FormField>
-      <FormField>
-        <Label htmlFor="imageUrl">Profile Image</Label>
-        <Input
+      </div>
+      <div>
+        <label htmlFor="imageUrl">Profile Image</label>
+        <input
           type="text"
           id="imageUrl"
           value={imageUrl}
           onChange={(e) => setImageUrl(e.target.value)}
         />
-      </FormField>
-      <FormField>
-        <Label htmlFor="bio">Bio</Label>
-        <Textarea
+      </div>
+      <div>
+        <label htmlFor="bio">Bio</label>
+        <textarea
           rows="3"
           id="bio"
           value={bio}
           onChange={(e) => setBio(e.target.value)}
         />
-      </FormField>
-      <FormField>
-        <Button type="submit">{isLoading ? "Loading..." : "Sign Up"}</Button>
-      </FormField>
-      <FormField>
+      </div>
+      <div>
+        <button type="submit">{isLoading ? "Loading..." : "Sign Up"}</button>
+      </div>
+      <div>
         {errors.map((err) => (
-          <Error key={err}>{err}</Error>
+          <div key={err}>{err}</div>
         ))}
-      </FormField>
+      </div>
     </form>
   );
 }
 
-export default SignInForm;
+export default SignUpForm;
+
+
 
 
 
